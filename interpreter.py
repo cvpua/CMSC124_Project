8f335fd99ce8de4cc376f2fe977f7e3cf4b77c5b
rcmirandilla <rcmirandilla@up.edu.ph>
2020-11-27 01:30:21 +0800

    Add structure to the parser and analyzer

--- interpreter.py
+++ interpreter.py
@@ -1,103 +1,273 @@
+# TO DO
+# [ ] Parser: Function definition for every production rule in our grammar
+# [ ] Analyzer: Pag-cacatch ng mga instances na pwedeng magpabago sa symbol table at yung mga kailangang iexecute like GIMMEH and VISIBLE
+
+
 import re
 import tkinter
 from tkinter.filedialog import askopenfilename
 
-
-class Lol:
-      token_expressions = [
-        (r"^HAI\s", "Code Delimiter"),
-        (r"^KTHXBYE\s|^KTHXBYE$", "Code Delimiter"),
-        (r"^BTW\s", "Comment Identifier"),
-        (r"^OBTW\s", "Multilinecomment Delimiter"),
-        (r"^TLDR\s", "Multilinecomment Delimiter"),
-        (r"^I HAS A\s", "Variable Declaration"),
-        (r"^ITZ\s", "Variable Assignment"),
-        (r"^R\s", "KEYWORD"),
-        (r"^SUM OF\s", "Addition Keyword"),
-        (r"^DIFF OF\s", "Subtraction Keyword"),
-        (r"^PRODUKT OF\s", "Multiplication Keyword"),
-        (r"^QUOSHUNT OF\s", "Subtraction Keyword"),
-        (r"^MOD OF\s", "Modulo Keyword"),
-        (r"^BIGGR OF\s", "Max Keyword"),
-        (r"^SMALLR OF\s", "Min Keyword"),
-        (r"^BOTH OF\s", "And Keyword"),
-        (r"^EITHER OF\s", "Or Keyword"),
-        (r"^WON OF\s", "Xor Keyword"),
-        (r"^NOT\s", "Not Keyword"),
-        (r"^ANY OF\s", "KEYWORD"),
-        (r"^ALL OF\s", "KEYWORD"),
-        (r"^BOTH SAEM\s", "Comparison == Keyword"),
-        (r"^DIFFRINT\s", "Comparison != Keyword"),
-        (r"^SMOOSH\s", "String Concatenation Keyword"),
-        (r"^MAEK\s", "KEYWORD"),
-        (r"^A\s", "KEYWORD"),
-        (r"^IS NOW A\s", "KEYWORD"),
-        (r"^VISIBLE\s", "Output Keyword"),
-        (r"^GIMMEH\s", "Input Keyword"),
-        (r"^O RLY\?\s", "If-else Delimiter"),
-        (r"^YA RLY\s", "If Delimiter"),
-        (r"^MEBBE\s", "Elif Delimiter"),
-        (r"^NO WAI\s", "Else Delimiter"),
-        (r"^OIC\s", "If-else Delimiter or Switch Delimiter"),
-        (r"^WTF\?\s", "Switch Delimiter"),
-        (r"^OMG\s", "Case Delimiter"),
-        (r"^OMGWTF\s", "Default Case Delimiter"),
-        (r"^IM IN YR\s", "KEYWORD"),
-        (r"^UPPIN\s", "KEYWORD"),
-        (r"^NERFIN\s", "KEYWORD"),
-        (r"^YR\s", "KEYWORD"),
-        (r"^TIL\s", "KEYWORD"),
-        (r"^WILE\s", "KEYWORD"),
-        (r"^IM OUTTA YR\s", "KEYWORD"),
-        (r"^(-?[0-9]+)\s", "Numbr Literal"),
-        (r"^(-)?[0-9]*(\.)[0-9]+\s", "Numbar Literal"),
-        (r"^\".+\"\s", "Yarn Literal"),
-        (r"^(WIN|FAIL)\s", "Troof Literal"),
-        (r"^(NOOB|NUMBR|NUMBAR|YARN|TROOF)\s", "Type Literal"),
-        (r"^[a-zA-Z][a-zA-Z0-9_]*\s", "Variable Identifier"),
-      ]
-
-      tokens = []
+TOK_EXP = [
+    (r"^HAI\s", "HAI_KEYWORD"),
+    (r"^KTHXBYE\s|^KTHXBYE$", "KTHXBYE_KEYWORD"),
+    (r"^BTW\s", "BTW_KEYWORD"),
+    (r"^OBTW\s", "OBTW_KEYWORD"),
+    (r"^TLDR\s", "TLDR_KEYWORD"),
+    (r"^I HAS A\s", "I_HAS_A_KEYWORD"),
+    (r"^ITZ\s", "ITZ_KEYWORD"),
+    (r"^R\s", "R_KEYWORD"),
+    (r"^SUM OF\s", "SUM_OF_KEYWORD"),
+    (r"^DIFF OF\s", "DIFF_OF_KEYWORD"),
+    (r"^PRODUKT OF\s", "PRODUKT_OF_KEYWORD"),
+    (r"^QUOSHUNT OF\s", "QUOSHUNT_OF_KEYWORD"),
+    (r"^MOD OF\s", "MOD_OF_KEYWORD"),
+    (r"^BIGGR OF\s", "BIGGR_OF_KEYWORD"),
+    (r"^SMALLR OF\s", "SMALLR_OF_KEYWORD"),
+    (r"^BOTH OF\s", "BOTH_OF_KEYWORD"),
+    (r"^EITHER OF\s", "EITHER_OF_KEYWORD"),
+    (r"^WON OF\s", "WON_OF_KEYWORD"),
+    (r"^NOT\s", "NOT_KEYWORD"),
+    (r"^ANY OF\s", "ANY_OF_KEYWORD"),
+    (r"^ALL OF\s", "ALL_OF_KEYWORD"),
+    (r"^BOTH SAEM\s", "BOTH_SAEM_KEYWORD"),
+    (r"^DIFFRINT\s", "DIFFRINT_KEYWORD"),
+    (r"^SMOOSH\s", "SMOOSH_KEYWORD"),
+    (r"^MAEK\s", "MAEK_KEYWORD"),
+    (r"^A\s", "A_KEYWORD"),
+    (r"^IS NOW A\s", "IS_NOW_A_KEYWORD"),
+    (r"^VISIBLE\s", "VISIBLE_KEYWORD"),
+    (r"^GIMMEH\s", "GIMMEH_KEYWORD"),
+    (r"^O RLY\?\s", "O_RLY?_KEYWORD"),
+    (r"^YA RLY\s", "YA_RLY_KEYWORD"),
+    (r"^MEBBE\s", "MEBBE_KEYWORD"),
+    (r"^NO WAI\s", "NO_WAI_KEYWORD"),
+    (r"^OIC\s", "OIC_KEYWORD"),
+    (r"^WTF\?\s", "WTF?_KEYWORD"),
+    (r"^OMG\s", "OMG_KEYWORD"),
+    (r"^OMGWTF\s", "OMGWTF_KEYWORD"),
+    (r"^IM IN YR\s", "IM_IN_YR_KEYWORD"),
+    (r"^UPPIN\s", "UPPIN_KEYWORD"),
+    (r"^NERFIN\s", "NERFIN_KEYWORD"),
+    (r"^YR\s", "YR_KEYWORD"),
+    (r"^TIL\s", "TIL_KEYWORD"),
+    (r"^WILE\s", "WILE_KEYWORD"),
+    (r"^IM OUTTA YR\s", "IM_OUTTA_YR_KEYWORD"),
+    (r"^(-?[0-9]+)\s", "NUMBR_LITERAL"),
+    (r"^(-)?[0-9]*(\.)[0-9]+\s", "NUMBAR_LITERAL"),
+    (r"^\".+\"\s", "YARN_LITERAL"),
+    (r"^(WIN|FAIL)\s", "TROOF_LITERAL"),
+    (r"^(NOOB|NUMBR|NUMBAR|YARN|TROOF)\s", "TYPE_LITERAL"),
+    (r"^[a-zA-Z][a-zA-Z0-9_]*\s", "VAR_IDENTIFIER"),
+  ]
 
 
+class Interpreter:
+  def __init__(self):
+    self.tokens = []
+    self.text = ""
+    self.tree = None
+    self.symbol_table = {}
+  
+  def readFile(self):
+    # To do: 
+    # [ ] check if the file type is lol, if not, then print an error
+    filename = askopenfilename()
+    file = open(filename,'r')
+    self.text = file.read()
+  
+  def run_lexer(self):
+    if (self.text != ""):
+      lexer = Lexer(self.text, TOK_EXP)
+    else:
+      print("Please set the file first")
+      return False
+    tokens = lexer.tokenize()
+    if (tokens):
+      self.tokens = tokens
+    else:
+      return False
+  
+  # def run_parser(self):
+  #   if (len(self.tokens) != 0):
+  #     parser = Parser(self.tokens)
+  #   else:
+  #     print("The list of tokens is empty")
+  #     return False
+  #   self.tree = parser.parse()
+  #   return True
+  
+  # def run_analyzer(self):
+  #   if (self.tree != None):
+  #     analyzer = Analyzer(self.tree)
+  #   else:
+  #     print("The tree is still empty")
+  #     return False
+  #   analyzer.analyze()
+  #   self.symbol_table = analyzer.symbol_table
+  #   return True
+  
+  # def interpret(self):
+  #   is_successful = self.run_lexer()
+  #   if (not(is_successful)):
+  #     print("Tokenizing is not successful")
+  #     return False
 
-      def readFile(self):
-        filename = askopenfilename()
-        file = open(filename,'r')
-        self.text = file.read()
+  #   is_successful = self.run_parser()
+  #   if (not(is_successful)):
+  #     print("Parsing is not successful")
+  #     return False
+    
+  #   is_successful = self.run_analyzer()
+  #   if (not(is_successful)):
+  #     print("Semantic analysis is not successful")
+  #     return False
+  
+  def print_text(self):
+    print(self.text)
+  
+  def print_tokens(self):
+    for token in self.tokens:
+      print('"' + token.name + '"' + "\n\t" + token.type )
+  
+  # TO DO:
+  # [ ] Print Tree
+  # - this function will print the visualization of the parse tree
+  # def print_tree(self):
+  
+  # [ ] Print Symbol Table
+  # def print_symbol_table(self):
 
-      def printTokens(self):
-        for i in range(len(self.tokens)):
-              print(self.tokens[i][0] + " \t" + self.tokens[i][1] )
+class Token:
+  def __init__(self, name, type):
+    self.name = name
+    self.type = type
 
 
-      def lex(self):
-        tokens = self.tokens
-        text = self.text
-        token_expressions = self.token_expressions
+class Lexer:
+  def __init__(self, text, TOK_EXP):
+    self.text = text
+    self.token_expressions = TOK_EXP
 
-        lines = text.split("\n")
-        line_number = 1
-        for line in lines:
-          line = line.strip()
-          line = line + "\n"
-          while (line != "" and line != "\n"):
-            for token_exp in token_expressions:
-              pattern, tag = token_exp
-              match = re.match(pattern, line)
-              if (match):
-                tokens.append([match.group(0)[:-1],tag])
-                line = line[match.end(0):]
-                break
-            else:
-              print(f"Error in line number {line_number}; Invalid token")
-              return
-          line_number += 1
-        return tokens
+  def tokenize(self):
+    tokens = []
+    lines = self.text.split("\n")
+    line_number = 1
+    for line in lines:
+      line = line.strip()
+      line = line + "\n"
+      while (line != "" and line != "\n"):
+        for token_exp in self.token_expressions:
+          pattern, tag = token_exp
+          match = re.match(pattern, line)
+          if (match):
+            name = match.group(0)[:-1]
+            tokens.append(
+                Token(name,tag)
+              )
+            line = line[match.end(0):]
+            break
+        else:
+          print(f"Error in line number {line_number}; Invalid token")
+          return False
+      line_number += 1
+    return tokens
 
+class Node:
+  def __init__(self, type, value = None, children = None):
+    self.type = type
+    self.value = value
+    self.children = children
+
+class Parser:
+  def __init__(self, tokens):
+    self.tokens = tokens
+    self.current_token = tokens[0]
+  
+  def eat(self, token_type):
+    if (token_type == self.current_token.type):
+      self.tokens.pop()
+    if (len(self.tokens) != 0): 
+      self.current_token = self.tokens[0]
+    else: 
+      print("Tokens is empty")
+      return False
+    return True
+  
+  def codeblock(self):
+    children = []
+    
+    # Mahaba to
+    
+    return Node("CODEBLOCK", children)
+  
+  def program(self):
+    children = []
+    
+    if (self.current_token.type == "HAI_KEYWORD"):
+      self.eat("HAI_KEYWORD")
+      children.append(Node("HAI_KEYWORD"))
+    else:
+      print("Invalid Syntax: Expect HAI_KEYWORD but saw " + self.current_token.type)
+      return False
+    
+    self.codeblock()
+    
+    if (self.current_token.type == "KTHXBYE_KEYWORD"):
+      self.eat("KTHXBYE_KEYWORD")
+      children.append(Node("KTHXBYE_KEYWORD"))
+    else:
+      print("Invalid Syntax: Expect KTHXBYE_KEYWORD but saw " + self.current_token.type)
+      return False
+    
+    return Node("PROGRAM", children)
+  
+  def parse(self):
+    return self.program()
+
+class Symbol:
+  def __init__(self, type, value):
+    self.type = type
+    self.value = value
+    
+
+class Analyzer:
+  def __init__(self, tree):
+    self.tree = tree
+    self.symbol_table = {}
+  
+  def analyze(self, current_node):
+    # set conditions for all nodes that change the value of a variable or declare it
+    # for example:
+    if (current_node.type == "VAR_INIT"):
+      # thinking...antok na ako, basta ivivisit niya yung children then kukunin niya yung variable identifier and yung literal
+      # pagkakuha ng value, icacall si insert, name = variable identifier, type = value.type, value = literal.value
+      print()
       
       
-lol = Lol()
+      
+    # Mahaba to 
+      
+    
+    
+    for child in current_node.children:
+      self.analyze(child)
+  
+  # Adding a new variable or updating the value of the variable
+  def insert(self, name, type, value = None):
+    new_symbol = Symbol(type, value)
+    self.symbol_table.update({
+      name: new_symbol
+    })
+  
+  # This function is for knowing whether the current variable is already declared
+  def lookup(self, name):
+    for key in self.symbol_table:
+      if (key == name):
+        return True
+    return False
+    
+      
+lol = Interpreter()
 lol.readFile()
-lol.lex()
-lol.printTokens()
+lol.run_lexer()
+lol.print_tokens()
--- sample.lol
+++ sample.lol
@@ -11,32 +11,29 @@ HAI 1.2
 
   I HAS A year
 
-  IT R choice   BTW IT = choice
-  WTF?          BTW WTF? uses IT variable
+  IT R choice
+  WTF?
   OMG 1
     VISIBLE "enter year"
     GIMMEH year
 
-    BTW 2020-year
     VISIBLE DIFF OF 2020 AN year " years old"
     GTFO
   OMG 2
     VISIBLE "enter year"
     GIMMEH year
     
-    BTW (2020-year)*12
     VISIBLE PRODUKT OF DIFF OF 2020 AN year AN 12 " months old"
     GTFO
   OMGWTF
     VISIBLE "choice is not 1 or 2"
   OIC
 
-  BTW choice!=1 && choice!=2 && choice!=3
   ALL OF DIFFRINT choice AN 1 AN DIFFRINT choice AN 2 AN DIFFRINT choice AN 3
 
   O RLY?
     YA RLY
-      VISIBLE "invalid input!" BTW if choice is not 1,2,3 then invalid input
+      VISIBLE "invalid input!"
     NO WAI
       VISIBLE "goodbye!"
   OIC
